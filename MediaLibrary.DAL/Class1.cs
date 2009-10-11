e9c5c9c4dc23709f53f36e62ae826e75a34708c6
Dave Mateer <davemateer@gmail.com>
2009-10-12 08:09:41 +1300

    first commit

---
+++ MediaLibrary.DAL/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace MediaLibrary.DAL
+{
+    [TestFixture]
+    public class SqlConnectionFixture
+    {
+        [Test]
+        public void ConnectionIsOpen()
+        {
+            string connectionString = ConfigurationSettings.AppSettings.Get("Catalog.Connection");
+            Assert.IsNotNull(connectionString);
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Assert.AreEqual(ConnectionState.Open, connection.State);
+            connection.Close();
+        }
+    }
+}
